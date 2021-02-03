a0d38817ac6942c914f31af16893fdbac96a7c44
Gustavo Verdesoto <44102561+1805140793@users.noreply.github.com>
2021-02-03 13:24:24 -0500

    constructor

--- app/src/main/java/com/example/consultasqlserver/Producto.java
+++ app/src/main/java/com/example/consultasqlserver/Producto.java
@@ -5,4 +5,9 @@ public class Producto {
     private String producto;
     private float precio;
 
+    public Producto(int codigo, String producto, float precio) {
+        this.codigo = codigo;
+        this.producto = producto;
+        this.precio = precio;
+    }
 }
