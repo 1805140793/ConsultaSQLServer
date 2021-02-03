b9837fcef17d2704a217e9b87582c3b3c287ca96
Gustavo Verdesoto <44102561+1805140793@users.noreply.github.com>
2021-02-03 13:26:10 -0500

    Clase persona

---
+++ app/src/main/java/com/example/consultasqlserver/Persona.java
@@ -0,0 +1,11 @@
+package com.example.consultasqlserver;
+
+public class Persona {
+    private int id;
+    private String nombre;
+
+    public Persona(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+}
